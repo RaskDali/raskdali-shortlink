d987420a7e3a3f3561efa581737d49d56a6d79dd
RaskDali <perkampl@gmail.com>
2025-08-06 03:50:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
